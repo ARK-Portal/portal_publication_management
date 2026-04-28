f373c388b2ca928b7f2a783b5e9dd006371e522a
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2026-04-28 17:44:49 -0500

    add process_pubmed_result from utils.py, parse authors and first author surname from pubmed xml data, import utils func to make them accessible

--- utils/query_pubmed.py
+++ utils/query_pubmed.py
@@ -9,13 +9,16 @@ A set of functions to pubmed for Accelerating Medicines Partnership publications
 # import necessary libraries
 import sys
 import requests
+import json
 import xml.etree.ElementTree as ET
 import pandas as pd
+from datetime import datetime
+from utils import *
 
-def query_pubmed_ids(ignore_ids):
+def query_pubmed_ids(ignore_ids, token):
   query = ["https://eutils.ncbi.nlm.nih.gov/", 
            "entrez/eutils/esearch.fcgi?db=pubmed",
-           "&api_key=", ncbiapikey,
+           "&api_key=", token,
            "&retmax=1000",
            "&term=Accelerating+Medicines+Partnership+AND+", 
            "2025", "[pdat]"]
@@ -29,39 +32,89 @@ def query_pubmed_ids(ignore_ids):
   pubmed_ids = list(set(pubmed_ids))
   
   # remove pubmed ids already cataloged in ARK portal
-  pubmed_ids = [x for x pubmed_ids if x not in ignore_ids]
+  pubmed_ids = [x for x in pubmed_ids if x not in ignore_ids]
   
   return(pubmed_ids)
 
-def pmid_to_doi(ids):
+def get_pubmed_metadata(ids, token):
   uid_list = ",".join(ids)
   #esummary.fcgi?db=<database>&id=<uid_list>
   query = ["https://eutils.ncbi.nlm.nih.gov/", 
            "entrez/eutils/esummary.fcgi?db=pubmed",
-           "&api_key=", ncbiapikey,
+           "&api_key=", token,
            "&id=", uid_list]
   query = ''.join(query)
   query_response = requests.get(query)
   root = ET.fromstring(query_response.content)
+  data = get_xml_grandchild(root)
   
+  with open('metadata_translation.json', 'r') as file:
+    metadata_translation = json.load(file)
+  
+  results = {'Source': [], 'authors': [], 'FirstAuthorSurname': []}
+  for k in metadata_translation.keys():
+    results[k] = []
+  
+  for id in data:
+    for k in results.keys():
+      if k in data[id].keys():
+        #print(f"{k} found, adding {data[id][k]} to results.")
+        results[k].append(data[id][k])
+      else:
+        results[k].append('Not Available')
+  
+  results = pd.DataFrame(results)
+  results['PMID'] = list(data.keys())
+  results = process_pubmed_results(results, trans = metadata_translation)
+  
+  return(results)
+
+def get_xml_grandchild(root):
   data = {}
   for child in root:
     id = child.find('Id').text
     data[id] = {}
     for gchild in child.iter('Item'):
-      #name = gchild.find('name').text
       name = gchild.get('Name')
-      #print(id, gchild.text, name)
-      data[id][name] = gchild.text
+      if name == 'AuthorList':
+        authors = []
+        for ggchild in gchild.iter("Item"):
+          if ggchild.text != '\n\t\t':
+            authors.append(ggchild.text)
+        data[id]['authors'] = ", ".join(authors)
+        # get surname of first author
+        first_auth = authors[0]
+        #first_auth.split()[0]
+        data[id]['FirstAuthorSurname'] = first_auth.split()[0]
+        
+      else:
+        data[id][name] = gchild.text
   
-  doi = []
-  for id in data:
-    if 'doi' in data[id].keys():
-      doi.append(data[id]['doi'])
-    else:
-      doi.append(None)
-    
-  return(doi)
+  return(data)
+
+def process_pubmed_results(results, trans):
+  results['year'] = results['PubDate'].apply(lambda x: x.split()[0])
+  x = ['FirstAuthorSurname', 'Source', 'year', 'PMID']
+  results['name'] = results[x].apply(lambda row: " ".join([row['FirstAuthorSurname'], row['Source'], row['year'], row['PMID']]), axis = 1)
+  results = results.rename(columns = trans)
+  
+  # add additional columns to fill out annotation template
+  ## guess at program label
+  results['program'] = results['authors'].apply(lambda x:guess_program(x))
+  results['URL'] = [''.join(["https://doi.org/", x]) if re.search('10', x) else None for x in results['DOI']]
+  ## guess at publicationType
+  results['publicationType'] = 
+  
+  # modify identifiers for synapse registry match
+  results['PMID'] = [''.join(['PMID:', x]) for x in results['PMID']]
+  results['PMCID'] = [''.join(['pmc:', x]) for x in results['PMCID']]
+  
+  anno_template = get_ark_pub_anno_template()
+  keep = [x for x in list(results.columns) if x in list(anno_template.columns)]
+  results = results.loc[:, keep]
+  add = [x for x in list(anno_template.columns) if x not in list(results.columns)]
+  
+  return(results)
 
 
 # END
