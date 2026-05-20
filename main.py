79413562694ffe683505a2307b45bc18b6f23bf9
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2026-05-20 15:33:59 -0500

    add scope param to main to allow execution for different project/program scopes, add comments to help with interpretation

--- main.py
+++ main.py
@@ -17,17 +17,21 @@ from synapse import *
 
 # find new pubs, update pub metadata
 # write to csv for human-in-the-loop review and PR merge
-def main():
-  
-  ncbiapikey = get_ncbi_api_key()
-  
-  all_ark_pubs = get_all_pubs() # returns dict of PMID and DOI lists
-  
-  pubmed_ids = query_pubmed_ids(ignore_ids = all_ark_pubs['PMID'], token = ncbiapikey)
-  results = get_pubmed_metadata(pubmed_ids, token = ncbiapikey)
-  
-  results = query_crossref(results)
-  results = filter_results(results)
+def main(scope = None):
+  if scope is None:
+    print("please provide a 'scope' for excuting this function")
+  else:
+    ncbiapikey = get_ncbi_api_key()
+    
+    all_ark_pubs = get_all_pubs() # returns dict of PMID and DOI lists for stuff already tracked in backend
+    
+    # perform broad query in pubmed for AMP publications
+    pubmed_ids = query_pubmed_ids(ignore_ids = all_ark_pubs['PMID'], token = ncbiapikey)
+    # using returned pubmed ids query pubmed for publication metadata
+    results = get_pubmed_metadata(pubmed_ids, token = ncbiapikey, scope)
+    
+    results = query_crossref(results)
+    results = filter_results(results)
 
 if __name__ == "__main__":
   # calling main function
