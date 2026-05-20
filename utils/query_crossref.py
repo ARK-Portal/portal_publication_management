66763b22fb0635011614399214664a99121c4bbe
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2026-05-20 17:52:20 -0500

    enable ETL for different query results

--- utils/query_crossref.py
+++ utils/query_crossref.py
@@ -19,15 +19,17 @@ def query_crossref(doi):
     out = json.load(file)['crossref']
 
   for d in doi:
+    print(d)
     query = ["https://api.crossref.org/works/doi/", d]
     query = ''.join(query)
     query_response = requests.get(query)
     results = json.loads(query_response.content)['message']
+    journal_dict = get_journal(results)
     
-    pubDate = results['posted']['date-parts'][0]
-    journal = results['institution'][0]['name']
-    out['journal'].append(journal)
+    pubDate = results['published']['date-parts'][0]
     out['year'].append(pubDate[0])
+    
+    out['journal'].append(journal_dict['journal'])
     out['title'].append(results['title'][0])
     
     pubDate = [str(x) for x in pubDate]
@@ -36,7 +38,10 @@ def query_crossref(doi):
     
     authors = process_cr_authors(authors = results['author'])
     out['authors'].append(authors)
-    out['name'] = " ".join([results['author'][0]['family'], journal, "TBD"])
+    out['name'] = " ".join([results['author'][0]['family'], journal_dict['journal_short'], "TBD"])
+  
+  out = pd.DataFrame(out)
+  return(out)
 
 def process_cr_authors(authors):
   out = []
@@ -50,7 +55,19 @@ def process_cr_authors(authors):
   out = ", ".join(out)
   return(out)
 
-
+def get_journal(results):
+  translate = {'bioRxiv': 'Biorxiv : The Preprint Server For Biology', 
+               'medRxiv': 'Medrxiv : The Preprint Server For Health Sciences'}
+  
+  if 'institution' in results.keys():
+    journal_short = results['institution'][0]['name']
+    journal = translate[journal_short]
+  else:
+    journal_short = results['short-container-title'][0]
+    journal = results['container-title'][0]
+  
+  out = {'journal': journal, 'journal_short': journal_short}
+  return(out)
 
 
 
