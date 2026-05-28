24b849a4c949a8348de224ebb3958450b207508c
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2026-05-28 11:43:27 -0500

    append critical data to pub updates; write TBD pubs to file to assist with PR review and query results refinement

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import requests
 import xml.etree.ElementTree as ET
 import pandas as pd
 sys.path.append("utils")
-from utils import get_ncbi_api_key
+from utils import get_ncbi_api_key, harmonize_pub_df
 from query_pubmed import query_pubmed
 from query_crossref import query_crossref
 from synapse import get_all_pubs
@@ -46,8 +46,23 @@ def main():
   pub_updates = results[results['DOI'].isin(doi) == True] # pubs with a DOI that we DO have tracked but don't yet have a PMID
   if pub_updates.shape[0] > 0:
     print(f"pub_updates.shape[0] publications updates.")
+    
+    df = all_ark_pubs['data']
+    df = df.loc[:, ['DOI', 'id', 'associatedDataset']]
+    pub_updates = pub_updates.drop(columns = ['associatedDataset'])
+    pub_updates = pd.merge(pub_updates, df, on = 'DOI', how = 'left')
+    pub_updates = harmonize_pub_df(pub_updates, add = ['id'])
+    
     fid = "publication_updates.csv"
     pub_updates.to_csv(fid, index = False)
+  
+  df = all_ark_pubs['data']
+  TBD_pubs = df[df['name'].str.contains("TBD") == True]
+  TBD_pubs = harmonize_pub_df(TBD_pubs, add = ['id'])
+  if TBD_pubs.shape[0] > 0:
+    print(f"TBD_pubs.shape[0] TBD publications are tracked in backend project already.")
+    fid = "TBD_publications.csv"
+    TBD_pubs.to_csv(fid, index = False)
 
 if __name__ == "__main__":
   # calling main function
