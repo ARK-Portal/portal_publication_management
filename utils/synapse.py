f9959e109177e8dd2908d95ed2e52c12cc1f549b
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2026-05-28 11:42:19 -0500

    mod query to return all columns and have func return whole df in addition to filtered lists to support creation of TBD_pubs output

--- utils/synapse.py
+++ utils/synapse.py
@@ -13,16 +13,15 @@ import pandas as pd
 import synapseutils
 from synapseclient.models import query, File, Table
 
-
 def get_all_pubs():
   '''
   returns dict of PMID and DOI lists for stuff already tracked in backend 
   entity view 'All Publications'
   '''
   syn = synapseclient.login()
-  df = query("SELECT id, DOI, PMID from syn64429484")
+  df = query("SELECT * from syn64429484")
   
-  out = {'PMID': [], 'DOI': []}
+  out = {'PMID': [], 'DOI': [], 'data': df}
   pmid = df[df.PMID.str.match("^PMID:") == True]
   out['PMID'] = list(set(list(pmid.PMID)))
   #out['PMID'] = [item.replace("PMID:", "") for item in out['PMID']]
